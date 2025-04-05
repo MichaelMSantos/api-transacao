16d9ba15da22b833713db0451a8c4dc8d7e027ce
Michael M Santos <mmss240610@gmail.com>
2025-04-04 21:17:55 -0300

    chore: initial springboot config

---
+++ src/main/java/com/backend/api_transacao/ApiTransacaoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.api_transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTransacaoApplication.class, args);
+	}
+
+}
